ec93c351efe4b037906d1ca4021c0a7d6d805396
Salman-S-Rahman <salmanmern@gmail.com>
2022-04-13 14:48:15 +0600

    log in page design done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,7 +5,10 @@ import Form from "react-bootstrap/Form";
 import { FcGoogle } from "react-icons/fc";
 import { SiFacebook } from "react-icons/si";
 import { ImGithub } from "react-icons/im";
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
 import { BiLockOpenAlt } from "react-icons/bi";
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -17,19 +20,19 @@ const Login = () => {
               <h1>Sign In !</h1>
             </div>
             <Row className="g-3">
-              <Col xs={12} xl={4}>
+              <Col xs={12} xl={4} className="g-4">
                 <div className="social-sign sign1">
                   <FcGoogle className="fs-2" />
                   <h5 className="d-inline-block ms-3 fw-bold">Google</h5>
                 </div>
               </Col>
-              <Col xs={12} xl={4}>
+              <Col xs={12} xl={4} className="g-4">
                 <div className="social-sign sign2">
                   <SiFacebook className="fs-2 text-primary" />
                   <h5 className="d-inline-block ms-3 fw-bold">Facebook</h5>
                 </div>
               </Col>
-              <Col xs={12} xl={4}>
+              <Col xs={12} xl={4} className="g-4">
                 <div className="social-sign sign3">
                   <ImGithub className="fs-2" />
                   <h5 className="d-inline-block ms-3 fw-bold">Github</h5>
@@ -41,25 +44,51 @@ const Login = () => {
             </div>
           </Col>
           <Row>
-            <Col md={7} className="mx-auto">
+            <Col md={7} className="mx-auto mt-3">
               <div className="login-form">
                 <Form>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Group
+                    className="mb-3 form-inner"
+                    controlId="formBasicEmail"
+                  >
+                    <Form.Label className="form-icon">
+                      <MdOutlineMarkEmailUnread className="fs-1 text-primary font-weight-light" />
+                    </Form.Label>
+                    <Form.Control
+                      type="email"
+                      autoComplete="off"
+                      spellCheck="false"
+                      placeholder="Enter Your Email Address"
+                    />
                   </Form.Group>
 
-                  <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <BiLockOpenAlt />
-                    <Form.Control type="password" placeholder="Password" />
+                  <Form.Group
+                    className="mb-3 form-inner"
+                    controlId="formBasicPassword"
+                  >
+                    <Form.Label className="form-icon">
+                      <BiLockOpenAlt className="fs-1 text-primary font-weight-light" />
+                    </Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Enter Your Secret Password"
+                    />
                   </Form.Group>
-                  <input type="password" name="" id="" />
+
+                  <div className="d-flex justify-content-between">
+                    <Link to="/home"> Forget Password?</Link>
+                    <Link to="/home">New User?</Link>
+                  </div>
 
                   <Button
                     variant="primary"
                     type="submit"
-                    className="d-block w-100"
+                    className="d-block w-100 mt-4 d-flex justify-content-between px-4"
                   >
-                    Submit
+                    <span>Sign In</span>
+                    <span>
+                      <BsArrowRight className="fs-2" />
+                    </span>
                   </Button>
                 </Form>
               </div>
